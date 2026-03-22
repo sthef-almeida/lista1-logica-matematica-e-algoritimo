c804eaba07dc16e554d0f156e07d770dfadc71c8
Sthefanny Almeida <111467412+sthef-almeida@users.noreply.github.com>
2026-03-22 14:47:17 -0300

    Add multiplication program in C

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main () { 
+
+    int num1, num2, resultado; 
+
+    printf("digite dois números inteiros: "); 
+
+    scanf("%d %d", &num1, &num2); 
+
+    //operação de multiplicação 
+
+     resultado = num1 * num2; 
+
+     printf("O resultado de %d * %d é: %d\n", num1, num2, resultado); 
+
+return 0; 
+
+} 
